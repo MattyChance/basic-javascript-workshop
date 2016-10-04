9f63fc79f2c9915a678120ac9c36e71784f5331b
MattyChance <chenmatty.wang@gmail.com>
2016-10-04 18:14:02 +0000

    updated the first function and created the second function

--- answers.js
+++ answers.js
@@ -1,5 +1,10 @@
 //This is answers.js
 //Function 1: return the first character of a string
 function firstLetter(str) {
-    return str[0];
+    return str.substring(0, 1);
+}
+
+//function 2: return the last character of a string
+function lastCharacter(str) {
+    return str.substring(str.length, str.length - 1);
 }
